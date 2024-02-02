4e88cc7d81df3ab5d3ac28793240bec30876e84e
unknown <isaacneal5@gmail.com>
2024-02-02 15:35:22 -0600

    HW

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -4,5 +4,11 @@ def real_estate():
     current_price = int(input())
     last_month_price = int(input())
     # Your code goes here
+    changeInPrice = current_price - last_month_price
+    mortgage = (current_price * 0.051) / 12.
+
+    print("This house is $" + str(current_price) +". The change is $"+ str(changeInPrice) +" since last month.")
+    print("The estimated monthly mortgage is $" + str(mortgage) +".")  
+   
 if __name__ == "__main__":
     real_estate()
--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -5,5 +5,10 @@ def simple_stats():
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+    product = num1 * num2 * num3 * num4
+    average = (num1 + num2 + num3 + num4) / 4
+    print(str(f'{product:.0f}') + " " + str(f'{average:.0f}'))
+    print(str(f'{product:.3f}') + " " + str(f'{average:.3f}'))
+
 if __name__ == "__main__":
     simple_stats()
